a103b3b7bd970da437e912544042ea27e699de80
KGyulaAI <71291570+KGyulaAI@users.noreply.github.com>
2023-03-03 11:15:52 +0100

    Add files via upload

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -15,42 +15,14 @@ using System.Windows.Shapes;
 
 namespace PRGNyelvek
 {
-    public enum Nyelvcsaladok { moduláris, strukturált, funkcionális, objektumorientált };
-    public class Nyelv
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
     {
-        string neve;
-        bool forditosE;
-        Nyelvcsaladok nyelvcsalad;
-        int megjelenesEve;
-        bool alacsonyszintuE;
-        int nepszeruseg2022;
-
-        public Nyelv(bool forditosE, Nyelvcsaladok nyelvcsalad)
+        public MainWindow()
         {
-            this.forditosE = forditosE;
-            this.nyelvcsalad = nyelvcsalad;
-        }
-
-        public bool ForditosE { get => forditosE; }
-        public Nyelvcsaladok Nyelvcsalad { get => nyelvcsalad; set => nyelvcsalad = value; }
-
-
-        /// <summary>
-        ///
-        /// </summary>
-        /// <returns>Ha a legnépszerűbb 3-ba tartozik</returns>
-        public bool NepszeruE()
-        {
-            return true;
-        }
-
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <returns>Az elmúlt 5 évben megjelent nyelvek</returns>
-        public List<Nyelv> UjNyelvek()
-        {
-            return new List<Nyelv> { };
+            InitializeComponent();
         }
     }
 }
